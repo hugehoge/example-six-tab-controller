cee934f1e67babb683c2f548660689d22c64bb4c
hugehoge <hugehoge42@gmail.com>
2021-08-29 00:16:39 +0900

    Add the comments on how it works

--- ExampleSixTabController/RootViewController.swift
+++ ExampleSixTabController/RootViewController.swift
@@ -15,6 +15,9 @@ class RootViewController: UIViewController {
     private func transitionToFirstView() {
         let firstVC = TabBarController()
 
+        // `UITabbarController` can fully display six tabs in the device where `UITraitCollection.horizontalSizeClass` is `.regular`, such as iPad.
+        // This workaround uses `UIViewController.setOverrideTraitCollection` to force set `UITabBarController.traitCollection`.
+        // The `traitCollection` settings using `setOverrideTraitCollection` requires affect from the parent `UIViewControlller`.
         setOverrideTraitCollection(UITraitCollection(horizontalSizeClass: .regular),
                                    forChild: firstVC)
 
